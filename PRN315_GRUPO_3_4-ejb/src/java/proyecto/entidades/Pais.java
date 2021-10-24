1f88b9726567819d42518dd785ba469465711559
Julio Castillo <cp99015@ues.edu.sv>
2021-10-23 21:59:48 -0600

    agregando la conexion a la base de datos

---
+++ PRN315_GRUPO_3_4-ejb/src/java/proyecto/entidades/Pais.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.entidades;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author adalc
+ */
+@Entity
+@Table(name = "pais")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Pais.findAll", query = "SELECT p FROM Pais p")
+    , @NamedQuery(name = "Pais.findByPaisId", query = "SELECT p FROM Pais p WHERE p.paisId = :paisId")
+    , @NamedQuery(name = "Pais.findByPais", query = "SELECT p FROM Pais p WHERE p.pais = :pais")
+    , @NamedQuery(name = "Pais.findByActivo", query = "SELECT p FROM Pais p WHERE p.activo = :activo")
+    , @NamedQuery(name = "Pais.findByFechaCreacion", query = "SELECT p FROM Pais p WHERE p.fechaCreacion = :fechaCreacion")})
+public class Pais implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "pais_id")
+    private BigDecimal paisId;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "pais")
+    private String pais;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "activo")
+    private int activo;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "fecha_creacion")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaCreacion;
+
+    public Pais() {
+    }
+
+    public Pais(BigDecimal paisId) {
+        this.paisId = paisId;
+    }
+
+    public Pais(BigDecimal paisId, String pais, int activo, Date fechaCreacion) {
+        this.paisId = paisId;
+        this.pais = pais;
+        this.activo = activo;
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public BigDecimal getPaisId() {
+        return paisId;
+    }
+
+    public void setPaisId(BigDecimal paisId) {
+        this.paisId = paisId;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public int getActivo() {
+        return activo;
+    }
+
+    public void setActivo(int activo) {
+        this.activo = activo;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (paisId != null ? paisId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Pais)) {
+            return false;
+        }
+        Pais other = (Pais) object;
+        if ((this.paisId == null && other.paisId != null) || (this.paisId != null && !this.paisId.equals(other.paisId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "proyecto.entidades.Pais[ paisId=" + paisId + " ]";
+    }
+    
+}
