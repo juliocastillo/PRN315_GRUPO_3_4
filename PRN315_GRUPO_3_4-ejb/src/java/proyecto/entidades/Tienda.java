0e5edbfa1cb8c20745ff68a462d62bd35aa10477
Dell <Dell@DESKTOP-SCGF2B0>
2021-11-02 02:53:08 -0600

    Funcion crear de la tabla tienda

--- PRN315_GRUPO_3_4-ejb/src/java/proyecto/entidades/Tienda.java
+++ PRN315_GRUPO_3_4-ejb/src/java/proyecto/entidades/Tienda.java
@@ -14,6 +14,8 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -49,6 +51,7 @@ public class Tienda implements Serializable {
     @Basic(optional = false)
     @NotNull
     @Column(name = "tienda_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private BigDecimal tiendaId;
     @Basic(optional = false)
     @NotNull
---
+++ PRN315_GRUPO_3_4-war/src/java/proyecto/forms/TiendaForm.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.forms;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import proyecto.ejb.DireccionFacadeLocal;
+import proyecto.ejb.TiendaFacadeLocal;
+import proyecto.entidades.Direccion;
+import proyecto.entidades.Tienda;
+
+/**
+ *
+ * @author Dell
+ */
+@Named(value = "tiendaForm")
+@SessionScoped
+public class TiendaForm implements Serializable {
+
+    @Inject
+    private DireccionFacadeLocal direccionFacade;
+
+    @Inject
+    private TiendaFacadeLocal tiendaFacade;
+    
+    private List<Direccion> direccionList;
+    
+    private List<Tienda> tiendaList;
+    
+    private Direccion direccion;
+    
+    private Tienda tiendaNueva;
+    
+    @PostConstruct
+    public void init(){
+        cargar();
+    }
+    public void cargar(){
+        try{
+            tiendaList = tiendaFacade.findAll();
+            direccionList = direccionFacade.findAll();
+        }catch(Exception e){
+            
+        }
+    }
+    public void guardar(){
+        tiendaNueva.setTiendaId(BigDecimal.valueOf(155D));
+        tiendaNueva.setDireccionId(direccion);
+        tiendaFacade.create(tiendaNueva);
+        cargar();
+    }
+
+    public TiendaForm() {
+        direccion = new Direccion();
+        tiendaNueva = new Tienda();
+    }
+
+    public List<Direccion> getDireccionList() {
+        return direccionList;
+    }
+
+    public void setDireccionList(List<Direccion> direccionList) {
+        this.direccionList = direccionList;
+    }
+
+    public List<Tienda> getTiendaList() {
+        return tiendaList;
+    }
+
+    public void setTiendaList(List<Tienda> tiendaList) {
+        this.tiendaList = tiendaList;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+    public Tienda getTiendaNueva() {
+        return tiendaNueva;
+    }
+
+    public void setTiendaNueva(Tienda tiendaNueva) {
+        this.tiendaNueva = tiendaNueva;
+    }
+    
+}
